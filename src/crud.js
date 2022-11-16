5db2c467647d890d6a7cdd3b64c6e53db8a549d7
Yuri3358 <yurialmeida917@gmail.com>
2022-11-16 18:48:48 -0300

    feat:  src, storing CRUD operators and model

---
+++ src/crud.js
@@ -0,0 +1,15 @@
+async function register(data) { //object
+    
+}
+
+async function list() {  //get method
+    
+}
+
+async function update(name) { //name for find 
+    
+}
+
+async function remove(name) { //name for find
+    
+}
